0d259daf90cfb9ca2e2e84e51b2db6885e7da35c
João Moura <joao.moura@b-i.com>
2020-05-25 00:52:01 +0100

    Udated mith_router to latest version

--- example/deps.ts
+++ example/deps.ts
@@ -1,6 +1,6 @@
 export { default as debug } from 'https://deno.land/x/debuglog/debug.ts'
 export { Mith } from  'https://deno.land/x/mith@v0.1.1/mod.ts'
-export { Router } from 'https://deno.land/x/mith_router@v0.0.4/mod.ts'
+export { Router } from 'https://deno.land/x/mith_router@v0.0.7/mod.ts'
 export { serveStatic } from '../static.ts'
 export { cookieSession } from '../cookieSession.ts'
 export { resolve } from "https://deno.land/std@0.51.0/path/mod.ts"
